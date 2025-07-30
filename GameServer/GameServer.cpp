7d6a56ec60976f9173921d5aa8fb6d1c3e4541c6
JJW <90325769+JJW8584@users.noreply.github.com>
2025-07-31 00:26:36 +0900

    DB Bind

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -18,6 +18,7 @@
 #include <functional>
 #include "DBConnectionPool.h"
 #include "DBConnectionPool.h"
+#include "DBBind.h"
 
 enum
 {
@@ -47,6 +48,7 @@ int main()
 
 	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDB;Trusted_Connection=Yes;"));
 
+
 	// Create Table
 	{
 		auto query = L"										\
@@ -54,7 +56,9 @@ int main()
 			CREATE TABLE [dbo].[Gold]						\
 			(												\
 				[id] INT NOT NULL PRIMARY KEY IDENTITY,		\
-				[gold] INT NULL								\
+				[gold] INT NULL,							\
+				[name] NVARCHAR(50) NULL,					\
+				[createDate] DATETIME NULL					\
 			);";
 
 		DBConnection* dbConn = GDBConnectionPool->Pop();
@@ -66,44 +70,101 @@ int main()
 	for (int32 i = 0; i < 3; i++)
 	{
 		DBConnection* dbConn = GDBConnectionPool->Pop();
+
+		DBBind<3, 0> dbBind(*dbConn, L"INSERT INTO [dbo].[Gold]([gold], [name], [createDate]) VALUES(?, ?, ?)");
+
+		int32 gold = 100;
+		WCHAR name[100] = L"사용자1";
+		TIMESTAMP_STRUCT ts = { 2025, 7, 30 };
+		dbBind.BindParam(1, name);
+		dbBind.BindParam(0, gold);
+		dbBind.BindParam(2, ts);
+
+		ASSERT_CRASH(dbBind.Execute());
+
+		/*
 		// 기존 바인딩된 정보 날림
 		dbConn->Unbind();
 
 		int32 gold = 100;
 		SQLLEN len = 0;
+
+		WCHAR name[100] = L"사용자1";
+		SQLLEN nameLen = 0;
+
+		TIMESTAMP_STRUCT ts = {};
+		ts.year = 2025;
+		ts.month = 7;
+		ts.day = 30;
+		SQLLEN tsLen = 0;
+
 		// 넘길 인자 바인딩
-		ASSERT_CRASH(dbConn->BindParam(1, SQL_C_LONG, SQL_INTEGER, sizeof(gold), &gold, &len));
+		ASSERT_CRASH(dbConn->BindParam(1, &gold, &len));
+		ASSERT_CRASH(dbConn->BindParam(2, name, &nameLen));
+		ASSERT_CRASH(dbConn->BindParam(3, &ts, &tsLen));
 
 		// SQL 실행
-		ASSERT_CRASH(dbConn->Execute(L"INSERT INTO [dbo].[Gold]([gold]) VALUES(?)"));
+		ASSERT_CRASH(dbConn->Execute(L"INSERT INTO [dbo].[Gold]([gold], [name], [createDate]) VALUES(?, ?, ?)"));
+		*/
+		
 		GDBConnectionPool->Push(dbConn);
 	}
 
 	// Read
 	{
 		DBConnection* dbConn = GDBConnectionPool->Pop();
+
+		DBBind<1, 4> dbBind(*dbConn, L"SELECT id, gold, name, createDate FROM [dbo].[Gold] WHERE gold = (?)");
+
+		int32 gold = 100;
+		dbBind.BindParam(0, gold);
+
+		int32 outId = 0;
+		int32 outGold = 0;
+		WCHAR outName[100];
+		TIMESTAMP_STRUCT outDate = {};
+		dbBind.BindCol(0, OUT outId);
+		dbBind.BindCol(1, OUT outGold);
+		dbBind.BindCol(2, OUT outName);
+		dbBind.BindCol(3, OUT outDate);
+
+		ASSERT_CRASH(dbBind.Execute());
+
+		/*
 		// 기존 바인딩된 정보 날림
 		dbConn->Unbind();
 
 		int32 gold = 100;
 		SQLLEN len = 0;
 		// 넘길 인자 바인딩
-		ASSERT_CRASH(dbConn->BindParam(1, SQL_C_LONG, SQL_INTEGER, sizeof(gold), &gold, &len));
+		ASSERT_CRASH(dbConn->BindParam(1, &gold, &len));
 
 		int32 outId = 0;
 		SQLLEN outIdLen = 0;
-		ASSERT_CRASH(dbConn->BindCol(1, SQL_C_LONG, sizeof(outId), &outId, &outIdLen));
+		ASSERT_CRASH(dbConn->BindCol(1, &outId, &outIdLen));
 
 		int32 outGold = 0;
 		SQLLEN outGoldLen = 0;
-		ASSERT_CRASH(dbConn->BindCol(2, SQL_C_LONG, sizeof(outGold), &outGold, &outGoldLen));
+		ASSERT_CRASH(dbConn->BindCol(2, &outGold, &outGoldLen));
+
+		WCHAR outName[100];
+		SQLLEN outNameLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(3, outName, len32(outName), &outNameLen));
+
+		TIMESTAMP_STRUCT outDate = {};
+		SQLLEN outDateLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(4, &outDate, &outDateLen));
+
 
 		// SQL 실행
-		ASSERT_CRASH(dbConn->Execute(L"SELECT id, gold FROM [dbo].[Gold] WHERE gold = (?)"));
+		ASSERT_CRASH(dbConn->Execute(L"SELECT id, gold, name, createDate FROM [dbo].[Gold] WHERE gold = (?)"));
+		*/
 
+		std::wcout.imbue(locale("kor"));
 		while (dbConn->Fetch())
 		{
-			cout << "Id : " << outId << " Gold : " << outGold << endl;
+			std::wcout << "Id : " << outId << " Gold : " << outGold << " name : " << outName << endl;
+			std::wcout << "Date : " << outDate.year << '/' << outDate.month << '/' << outDate.day << endl;
 		}
 
 		GDBConnectionPool->Push(dbConn);
