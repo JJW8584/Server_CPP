cc3215140c1f9922c82bd17af49c781c17277637
JJW <jjw8584@gmail.com>
2025-08-07 21:40:38 +0900

    delete db test code

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -47,44 +47,6 @@ void DoWorkerJob(ServerServiceRef& service)
 
 int main()
 {
-	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDB;Trusted_Connection=Yes;"));
-
-	DBConnection* dbConn = GDBConnectionPool->Pop();
-	DBSynchronizer dbSync(*dbConn);
-	dbSync.Synchronize(L"GameDB.xml");
-
-	{
-		WCHAR name[] = L"Player";
-		SP::InsertGold insertGold(*dbConn);
-		insertGold.In_Gold(100);
-		insertGold.In_Name(name);
-		insertGold.In_CreateDate(TIMESTAMP_STRUCT{ 2025, 8, 3 });
-		insertGold.Execute();
-	}
-
-	{
-		SP::GetGold getGold(*dbConn);
-		getGold.In_Gold(100);
-
-		int32 id = 0;
-		int32 gold = 0;
-		WCHAR name[100];
-		TIMESTAMP_STRUCT date;
-
-		getGold.Out_Id(OUT id);
-		getGold.Out_Gold(OUT gold);
-		getGold.Out_Name(OUT name);
-		getGold.Out_CreateDate(OUT date);
-
-		getGold.Execute();
-
-		while (getGold.Fetch())
-		{
-			GConsoleLogger->WriteStdOut(Color::BLUE,
-				L"ID[%d] Gold[%d] Name[%s]\n", id, gold, name);
-		}
-	}
-
 	ClientPacketHandler::Init();
 
 	ServerServiceRef service = MakeShared<ServerService>(
